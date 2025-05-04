de3fc73f3e171ba41e688da3d34738e77898af51
MRSgkce <mursidegokce985@gmail.com>
2025-05-04 23:53:18 +0300

    slider eklendi değerler sliderda değişince lable'da gösteriliyor.

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightlabel: UILabel!
+    @IBOutlet weak var heightlabel: UILabel!
+  
+    @IBOutlet weak var weight: UISlider!
+    @IBOutlet weak var height: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+  
+    
+    @IBAction func heightchang(_ sender: UISlider) {
+        
+        self.heightlabel.text = String(format: "%.1f", sender.value)
+    }
+    
+    @IBAction func weightchng(_ sender: UISlider) {
+        self.weightlabel.text = String(format: "%.0f", sender.value)
+    }
+    
 }
 
